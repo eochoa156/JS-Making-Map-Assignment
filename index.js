2efafdf5742a38a23b0d0e53ec5f547926d158d4
Emily Ochoa <eochoa156@gmail.com>
2024-02-07 12:18:47 -0800

    added selections and map div

---
+++ index.js
@@ -0,0 +1,11 @@
+/* 
+   things we need to do
+    - get user location
+    - create leaflet map
+    - after a category is selected (addEventListener)
+        - make a fetch request
+            -use the place search method
+            specify lat/long, categories and sort
+        - programatically render a list of results
+        -map the locations on the map
+*/
