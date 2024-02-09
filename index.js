5588dff24e34615726210c82cfbf49b7a32a44e7
Emily Ochoa <eochoa156@gmail.com>
2024-02-08 16:14:08 -0800

    map is on page

--- index.js
+++ index.js
@@ -9,6 +9,11 @@
         - programatically render a list of results
         -map the locations on the map
 */
+let map = L.map('map').setView([51.505, -0.09], 13)
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', { 
+    maxZoom: 19, 
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>' })
+    .addTo(map);
 
 navigator.geolocation.getCurrentPosition((success) => {
     console.log('it works:', position)
